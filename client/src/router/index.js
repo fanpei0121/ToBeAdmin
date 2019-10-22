2f8a1e661da266fcc6b5aae9abbdbf4e223a6899
fanpei <349119104@qq.com>
2019-10-22 15:04:35 +0800

    重写路由push方法

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,14 @@ import { constantRouterMap } from '@/config/router.config'
 
 Vue.use(Router)
 
+/**
+ * 重写路由的push方法
+ */
+const routerPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+  return routerPush.call(this, location).catch(error => error)
+}
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
